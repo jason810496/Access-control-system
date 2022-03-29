39dbd1671c2f7860080c9d7ee159c61940059757
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-03-29 15:01:36 +0800

    finish draft

--- MyAuth/MyAuth.ino
+++ MyAuth/MyAuth.ino
@@ -2,6 +2,12 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
+/* --- HTML FILE --- */
+#include "index.h"
+#include "login.h"
+#include "admin.h"
+#include "404.h"
+
 const char SSID[] = "Taylor";
 const char WIFIpassword[] = "Hh033229232";
 
@@ -28,70 +34,43 @@ bool is_authentified(){
   return false;
 }
 
-//login page, also called for disconnect
-
-void handleLogin(){
-  String msg;
-
-  
-
-  if (server.hasHeader("Cookie")){
-    Serial.print("Found cookie: ");
-    String cookie = server.header("Cookie");
-    Serial.println(cookie);
-
-    // is authentified 
-    if (cookie.indexOf("ESPSESSIONID=1") != -1) {
-      Serial.println("is authed & go to login page");
-
-      server.sendHeader("Location","/");
-      server.send(301);
-
-      Serial.println("redirect to main page");
-      return ;
-    }
-  }
-
-
-  if (server.hasArg("USERNAME") && server.hasArg("PASSWORD")){
-    if (server.arg("USERNAME") == "admin" &&  server.arg("PASSWORD") == "admin" ){
-      server.sendHeader("Location","/");
-      server.sendHeader("Cache-Control","no-cache");
-      server.sendHeader("Set-Cookie","ESPSESSIONID=1");
-      server.send(301);
-      Serial.println("Log in Successful");
-      return;
-    }
-  msg = "Wrong username/password! try again.";
-  Serial.println("Log in Failed");
-  }
-  String content = "<html><body><form action='/login' method='POST'>To log in, please use : admin/admin<br>";
-  content += "User:<input type='text' name='USERNAME' placeholder='user name'><br>";
-  content += "Password:<input type='password' name='PASSWORD' placeholder='password'><br>";
-  content += "<input type='submit' name='SUBMIT' value='Submit'></form>" + msg + "<br>";
-  content += "You also can go <a href='/inline'>here</a></body></html>";
-  server.send(200, "text/html", content);
-}
-
-//root page can be accessed only if authentification is ok
-void handleRoot(){
-  Serial.println("Enter handleRoot");
-  String header;
-  if (!is_authentified()){
-    server.sendHeader("Location","/login");
-    server.sendHeader("Cache-Control","no-cache");
-    server.send(301);
-    return;
-  }
-  String content = "<html><body><H2>hello, you successfully connected to esp8266!</H2><br>";
-  if (server.hasHeader("User-Agent")){
-    content += "the user agent used is : " + server.header("User-Agent") + "<br><br>";
-  }
-  content += "You can access this page until you <a href=\"/login?DISCONNECT=YES\">disconnect</a></body></html>";
-  server.send(200, "text/html", content);
-}
-
 /*---- Handle function ----*/
+
+void HandleRoot(){
+    server.send(200,"text/html",INDEX_HTML);
+}
+
+void HandleLogin(){
+
+    if (server.hasHeader("Cookie")){
+        Serial.print("Found cookie: ");
+        String cookie = server.header("Cookie");
+        Serial.println(cookie);
+
+        // is authentified 
+        if (cookie.indexOf("ESPSESSIONID=1") != -1) {
+            Serial.println("is authed & go to login page");
+
+            server.sendHeader("Location","/");
+            server.send(301);
+
+            Serial.println("redirect to main page");
+            return ;
+        }
+    }
+    // check password
+    if (server.hasArg("USERNAME") && server.hasArg("PASSWORD")){
+        if (server.arg("USERNAME") == USERNAME &&  server.arg("PASSWORD") == PASSWORD ){
+        server.sendHeader("Location","/admin");
+        server.sendHeader("Cache-Control","no-cache");
+        server.sendHeader("Set-Cookie","ESPSESSIONID=1");
+        server.send(301);
+        Serial.println("Log in Successful");
+        return;
+        }
+    }
+}
+
 void HandleAdmin(){
     Serial.println("Handle Admin");
     // check permission
@@ -105,25 +84,6 @@ void HandleAdmin(){
     server.send(200,"text/html",ADMIN_HTML);
 }
 
-void HandleRoot(){
-
-}
-
-void HandleLogin(){
-
-    // check password
-    if (server.hasArg("USERNAME") && server.hasArg("PASSWORD")){
-        if (server.arg("USERNAME") == USERNAME &&  server.arg("PASSWORD") == PASSWORD ){
-        server.sendHeader("Location","/");
-        server.sendHeader("Cache-Control","no-cache");
-        server.sendHeader("Set-Cookie","ESPSESSIONID=1");
-        server.send(301);
-        Serial.println("Log in Successful");
-        return;
-        }
-    }
-}
-
 void HandleLogout(){
     Serial.println("Disconnection");
     server.sendHeader("Location","/login");
@@ -134,7 +94,7 @@ void HandleLogout(){
 }
 
 void HandleNotFound(){
-
+    server.send(404, "text/html", String(ERROR_HTML));
 }
 /*---- Handle function end ----*/
 
@@ -154,43 +114,7 @@ void handleNotFound(){
   server.send(404, "text/plain", message);
 }
 
-void setup(void){
-  Serial.begin(9600);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-
-  server.on("/", handleRoot);
-  server.on("/login", handleLogin);
-  server.on("/inline", [](){
-    server.send(200, "text/plain", "this works without need of authentification");
-  });
-
-  server.onNotFound(handleNotFound);
-  //here the list of headers to be recorded
-  const char * headerkeys[] = {"User-Agent","Cookie"} ;
-  size_t headerkeyssize = sizeof(headerkeys)/sizeof(char*);
-  //ask server to track these headers
-  server.collectHeaders(headerkeys, headerkeyssize );
-
-
-  server.begin();
-  Serial.println("HTTP server started");
-}
-
-
-void setup2(){
+void setup(){
     // init Serial 
     Serial.begin(9600);
     // start connect to AP
